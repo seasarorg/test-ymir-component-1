4c81049b1dc2412f4198cff8d4e51ee23a3a08d6
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2006-05-23 09:36:37 +0000

    git-svn-id: https://www.seasar.org/svn/ymir/trunk@11 ce2cb714-bd0d-0410-8c66-e472ade7ad34

---
+++ framework/src/main/java/org/seasar/cms/framework/FrameworkServletContextListener.java
@@ -0,0 +1,16 @@
+package org.seasar.cms.framework;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class FrameworkServletContextListener implements ServletContextListener {
+
+    public void contextInitialized(ServletContextEvent event) {
+
+        
+    }
+
+    public void contextDestroyed(ServletContextEvent event) {
+
+    }
+}
