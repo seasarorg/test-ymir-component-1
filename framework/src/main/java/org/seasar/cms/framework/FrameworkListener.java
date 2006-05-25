798c2b61d7cb134562c48cac2201e26f1e2c7107
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2006-05-25 08:39:45 +0000

    ひとまずコミット。SPIKE中。
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@14 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- framework/src/main/java/org/seasar/cms/framework/FrameworkListener.java
+++ framework/src/main/java/org/seasar/cms/framework/FrameworkListener.java
@@ -2,14 +2,12 @@ package org.seasar.cms.framework;
 
 import javax.servlet.ServletContextEvent;
 
-import org.seasar.framework.container.factory.SingletonS2ContainerFactory;
 import org.seasar.framework.container.servlet.S2ContainerListener;
 
 public class FrameworkListener extends S2ContainerListener {
     
     public void contextInitialized(ServletContextEvent event) {
         
-        SingletonS2ContainerFactory
         super.contextInitialized(event);
     }
 }
