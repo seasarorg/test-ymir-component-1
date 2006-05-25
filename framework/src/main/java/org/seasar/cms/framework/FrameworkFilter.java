ca69fe540ea573213dc361c5d5f278f59fba633e
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2006-05-25 08:37:26 +0000

    ひとまずコミット。SPIKE中。
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@12 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- framework/src/main/java/org/seasar/cms/framework/FrameworkFilter.java
+++ framework/src/main/java/org/seasar/cms/framework/FrameworkFilter.java
@@ -9,21 +9,35 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
+import org.seasar.framework.container.ExternalContext;
+import org.seasar.framework.container.S2Container;
+import org.seasar.framework.container.factory.SingletonS2ContainerFactory;
+import org.seasar.framework.exception.EmptyRuntimeException;
+
 public class FrameworkFilter implements Filter {
 
-    public void init(FilterConfig arg0) throws ServletException {
-        // TODO Auto-generated method stub
-
-    }
-
-    public void doFilter(ServletRequest arg0, ServletResponse arg1,
-        FilterChain arg2) throws IOException, ServletException {
-        // TODO Auto-generated method stub
-
+    public void init(FilterConfig config) throws ServletException {
     }
 
     public void destroy() {
-        // TODO Auto-generated method stub
+    }
 
+    public void doFilter(ServletRequest request, ServletResponse response,
+        FilterChain chain) throws IOException, ServletException {
+
+        S2Container container = SingletonS2ContainerFactory.getContainer();
+        ExternalContext externalContext = container.getExternalContext();
+        if (externalContext == null) {
+            throw new EmptyRuntimeException("externalContext");
+        }
+        externalContext.setRequest(request);
+        externalContext.setResponse(response);
+
+        try {
+            chain.doFilter(request, response);
+        } finally {
+            externalContext.setRequest(null);
+            externalContext.setResponse(null);
+        }
     }
 }
