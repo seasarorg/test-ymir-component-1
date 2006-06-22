39498cb4861ad1828c3f37e14669c507c65fb9b0
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2006-06-22 05:38:22 +0000

    FrameworkUtilsにcopyPropertiesのバリエーションを追加した。
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@77 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- framework/src/main/java/org/seasar/cms/framework/util/FrameworkUtils.java
+++ framework/src/main/java/org/seasar/cms/framework/util/FrameworkUtils.java
@@ -1,12 +1,26 @@
 package org.seasar.cms.framework.util;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Iterator;
+import java.util.List;
 
 import org.apache.commons.beanutils.BeanUtils;
 
 public class FrameworkUtils {
 
     private FrameworkUtils() {
+    }
+
+    public static Object copyProperties(Class destClass, Object src) {
+
+        try {
+            return copyProperties(destClass.newInstance(), src);
+        } catch (InstantiationException ex) {
+            throw new RuntimeException(ex);
+        } catch (IllegalAccessException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
     public static Object copyProperties(Object dest, Object src) {
@@ -19,5 +33,18 @@ public class FrameworkUtils {
             ;
         }
         return dest;
+    }
+
+    public static Object copyProperties(Object[] dests, List srcList) {
+
+        Class destClass = dests.getClass().getComponentType();
+        if (dests.length < srcList.size()) {
+            dests = (Object[]) Array.newInstance(destClass, srcList.size());
+        }
+        int idx = 0;
+        for (Iterator itr = srcList.iterator(); itr.hasNext();) {
+            dests[idx++] = copyProperties(destClass, itr.next());
+        }
+        return dests;
     }
 }
