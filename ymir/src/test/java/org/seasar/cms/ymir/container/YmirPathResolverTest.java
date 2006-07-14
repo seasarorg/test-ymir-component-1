39945bc6105e852393b831a3ac071850d7508912
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2006-07-14 07:56:48 +0000

    テストケースを追加した。
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@186 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir/src/test/java/org/seasar/cms/ymir/container/YmirPathResolverTest.java
+++ ymir/src/test/java/org/seasar/cms/ymir/container/YmirPathResolverTest.java
@@ -1,6 +1,10 @@
 package org.seasar.cms.ymir.container;
 
 import java.net.URL;
+import java.net.URLClassLoader;
+
+import org.seasar.framework.container.S2Container;
+import org.seasar.framework.container.factory.S2ContainerFactory;
 
 import junit.framework.TestCase;
 
@@ -24,5 +28,32 @@ public class YmirPathResolverTest extends TestCase {
 
         // ## Assert ##
         assertNull(actual);
+    }
+
+    public void testDepends指定したものと直接指定したものが同じコンテナを指すこと() throws Exception {
+
+        // ## Arrange ##
+        // ## Act ##
+        S2Container container1;
+        S2Container container2;
+        URL testJarURL = getClass().getResource("test.jar");
+        URLClassLoader cl = new URLClassLoader(new URL[] { testJarURL },
+            getClass().getClassLoader());
+        ClassLoader old = Thread.currentThread().getContextClassLoader();
+        try {
+            Thread.currentThread().setContextClassLoader(cl);
+            S2Container rootContainer = S2ContainerFactory.create(getClass()
+                .getName().replace('.', '/')
+                + "_root.dicon");
+            container1 = rootContainer.getChild(0);
+            container2 = S2ContainerFactory.include(rootContainer, "jar:"
+                + testJarURL.toExternalForm()
+                + "!/META-INF/s2container/components.dicon");
+        } finally {
+            Thread.currentThread().setContextClassLoader(old);
+        }
+
+        // ## Assert ##
+        assertTrue(container1 == container2);
     }
 }
