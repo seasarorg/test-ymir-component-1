5fa75282022f3575371a052b527d1d05f7fa4382
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2007-01-08 23:35:57 +0000

    コメントを追記した。
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@548 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir/src/main/java/org/seasar/cms/ymir/response/scheme/impl/HtmlStrategy.java
+++ ymir/src/main/java/org/seasar/cms/ymir/response/scheme/impl/HtmlStrategy.java
@@ -4,6 +4,13 @@ import org.seasar.cms.ymir.Response;
 import org.seasar.cms.ymir.response.SelfContainedResponse;
 import org.seasar.cms.ymir.response.scheme.Strategy;
 
+/**
+ * このクラスは後方互換性のために存在します。通常は代わりにContentStrategyを利用すべきです。
+ * 具体的には、「<code>html:XXX</code>」と書く代わりに「<code>content:XXX</code>」
+ * もしくは「<code>content:text/html; charset=CCC:XXX</code>」と書くべきです。
+ * 
+ * @author YOKOTA Takehiko
+ */
 public class HtmlStrategy implements Strategy {
 
     public static final String SCHEME = "html";
