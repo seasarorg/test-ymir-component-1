168a73859aaee1e9645166379bef843eb98f0786
nahi <nahi@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2007-01-19 12:49:11 +0000

    avoid dependency on map elements ordering. (now it passes all tests on my environment)
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@559 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir/src/test/java/org/seasar/cms/ymir/PathTest.java
+++ ymir/src/test/java/org/seasar/cms/ymir/PathTest.java
@@ -1,6 +1,8 @@
 package org.seasar.cms.ymir;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import junit.framework.TestCase;
@@ -13,9 +15,34 @@ public class PathTest extends TestCase {
         Map map = new HashMap();
         map.put("a", new String[] { "A", "B" });
         map.put("c", new String[] { "C", "D" });
+        String expected = "/path/to/page?a=A&a=B&c=C&c=D";
 
-        Path target = new Path("/path/to/page", map);
+        Path expectedPath = new Path(expected);
+        Path actualPath = new Path("/path/to/page", map);
 
-        assertEquals("/path/to/page?a=A&a=B&c=C&c=D", target.asString());
+        assertEquals(expectedPath, actualPath);
+    }
+
+    private void assertEquals(Path expected, Path actual) {
+        Map expectedParameter = expected.getParameterMap();
+        Map actualParameter = actual.getParameterMap();
+        assertEquals(expectedParameter.size(), actualParameter.size());
+        for (Iterator itr = expectedParameter.keySet().iterator(); itr
+                .hasNext();) {
+            String key = (String) itr.next();
+            assertNotNull(actualParameter.get(key));
+            String[] expectedValue = (String[]) expectedParameter.get(key);
+            String[] actualValue = (String[]) actualParameter.get(key);
+            Arrays.sort(expectedValue);
+            Arrays.sort(actualValue);
+            assertEquals(expectedValue, actualValue);
+        }
+    }
+
+    private void assertEquals(String[] expected, String[] actual) {
+        assertEquals(expected.length, actual.length);
+        for (int idx = 0; idx < expected.length; ++idx) {
+            assertEquals(expected[idx], actual[idx]);
+        }
     }
 }
