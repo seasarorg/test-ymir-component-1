6df892f1c59871e1b78baf9bfb12eb9ec2754914
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2007-03-17 06:57:35 +0000

    [#CMS-29]に対処した。
    また、リダイレクトパスとして「.」と指定することで自分自身にリダイレクトできるようにした。（空文字列はYmirの世界ではコンテキストルートを表すので）
    
    
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@626 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir/src/main/java/org/seasar/cms/ymir/impl/DefaultRedirectionPathResolver.java
+++ ymir/src/main/java/org/seasar/cms/ymir/impl/DefaultRedirectionPathResolver.java
@@ -5,6 +5,8 @@ import org.seasar.cms.ymir.Request;
 
 public class DefaultRedirectionPathResolver implements RedirectionPathResolver {
 
+    public static final String SELF = ".";
+
     public String resolve(String path, Request request) {
         if (path == null) {
             return null;
@@ -12,9 +14,13 @@ public class DefaultRedirectionPathResolver implements RedirectionPathResolver {
 
         if (path.startsWith("/")) {
             path = request.getContextPath() + path;
-        } else {
-            // pathが「;a=b」などの場合。
+        } else if (path.length() == 0 || path.startsWith("?")
+                || path.startsWith(";")) {
+            // 空パスへのリダイレクトはYmirの世界ではルートパスへのリダイレクトとみなすのでこうしている。
             path = request.getContextPath() + "/" + path;
+        } else if (path.startsWith(SELF)) {
+            // 自分自身へのリダイレクト。
+            path = path.substring(SELF.length());
         }
 
         return path;
