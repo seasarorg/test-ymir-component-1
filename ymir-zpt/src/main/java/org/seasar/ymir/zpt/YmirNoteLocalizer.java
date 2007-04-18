a0627f34d9f9603bc14904b97f5b0c7d7928fdc8
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2007-04-18 09:13:45 +0000

    PathResolverを整理した。
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@718 ce2cb714-bd0d-0410-8c66-e472ade7ad34

---
+++ ymir-zpt/src/main/java/org/seasar/ymir/zpt/YmirNoteLocalizer.java
@@ -0,0 +1,102 @@
+package org.seasar.ymir.zpt;
+
+import java.text.MessageFormat;
+import java.util.Locale;
+
+import org.seasar.framework.container.ComponentNotFoundRuntimeException;
+import org.seasar.ymir.Globals;
+import org.seasar.ymir.MessageNotFoundRuntimeException;
+import org.seasar.ymir.Messages;
+import org.seasar.ymir.MessagesNotFoundRuntimeException;
+import org.seasar.ymir.Request;
+import org.seasar.ymir.Ymir;
+import org.seasar.ymir.YmirContext;
+
+import net.skirnir.freyja.TemplateContext;
+import net.skirnir.freyja.VariableResolver;
+import net.skirnir.freyja.zpt.tales.NoteLocalizer;
+
+public class YmirNoteLocalizer implements NoteLocalizer {
+    private static final String PROPERTYPREFIX_LABEL = "label.";
+
+    public String getMessageResourceValue(TemplateContext context,
+            VariableResolver varResolver, String noteValue,
+            Object[] noteParameters) {
+        if (noteValue == null) {
+            return null;
+        }
+
+        String messagesName;
+        int slash = noteValue.indexOf('/');
+        if (slash >= 0) {
+            messagesName = noteValue.substring(0, slash);
+            noteValue = noteValue.substring(slash + 1);
+        } else {
+            messagesName = Globals.NAME_MESSAGES;
+        }
+
+        Messages messages;
+        try {
+            messages = findMessages(messagesName);
+        } catch (ClassCastException ex) {
+            throw new RuntimeException(
+                    "Not Messages Object: messages' name may be incorrect: key="
+                            + noteValue + ", messages' name=" + messagesName,
+                    ex);
+        } catch (ComponentNotFoundRuntimeException ex) {
+            throw new MessagesNotFoundRuntimeException(
+                    "Messages object not found: messages' name may be incorrect: key="
+                            + noteValue + ", messages' name=" + messagesName,
+                    ex).setMessagesName(messagesName).setMessageKey(noteValue);
+        }
+
+        if (messages != null) {
+            Locale locale = findLocale(context, varResolver);
+            String v = messages.getProperty(noteValue, locale);
+            if (v != null) {
+                for (int i = 0; i < noteParameters.length; i++) {
+                    if (noteParameters[i] instanceof String) {
+                        String localizedValue = messages.getProperty(
+                                PROPERTYPREFIX_LABEL + noteParameters[i],
+                                locale);
+                        if (localizedValue != null) {
+                            noteParameters[i] = localizedValue;
+                        }
+                    }
+                }
+                noteValue = MessageFormat.format(v, noteParameters);
+            } else {
+                StringBuffer sb = new StringBuffer();
+                sb.append("Message corresponding key ('").append(noteValue)
+                        .append("') does not exist in ");
+                if (messagesName != null) {
+                    sb.append("Messages ('").append(messagesName).append("')");
+                } else {
+                    sb.append("default Messages (").append(Globals.MESSAGES)
+                            .append(")");
+                }
+                throw new MessageNotFoundRuntimeException(sb.toString())
+                        .setMessagesName(messagesName).setMessageKey(noteValue)
+                        .setLocale(locale);
+            }
+        }
+
+        return noteValue;
+    }
+
+    Messages findMessages(String messagesName) {
+        Ymir ymir = YmirContext.getYmir();
+        if (ymir == null) {
+            return null;
+        }
+
+        return (Messages) ymir.getApplication().getS2Container().getComponent(
+                messagesName);
+    }
+
+    public Locale findLocale(TemplateContext context,
+            VariableResolver varResolver) {
+        return ((Request) varResolver.getVariable(context,
+                YmirVariableResolver.NAME_YMIRREQUEST)).getLocale();
+    }
+}
--- ymir-zpt/src/main/java/org/seasar/ymir/zpt/YmirTalesExpressionEvaluator.java
+++ ymir-zpt/src/main/java/org/seasar/ymir/zpt/YmirTalesExpressionEvaluator.java
@@ -1,12 +1,15 @@
 package org.seasar.ymir.zpt;
 
+import net.skirnir.freyja.zpt.tales.NoteLocalizer;
 import net.skirnir.freyja.zpt.webapp.ServletTalesExpressionEvaluator;
 
 public class YmirTalesExpressionEvaluator extends
         ServletTalesExpressionEvaluator {
-
     public YmirTalesExpressionEvaluator() {
-
-        addPathResolver(new YmirPathResolver());
+        NoteLocalizer noteLocalizer = new YmirNoteLocalizer();
+        addPathResolver(new YmirPathResolver().setNoteLocalizer(noteLocalizer))
+                .addPathResolver(
+                        new LocalizationPathResolver()
+                                .setNoteLocalizer(noteLocalizer));
     }
 }
