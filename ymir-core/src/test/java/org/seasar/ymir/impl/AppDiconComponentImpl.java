41f2c824cc6bd47c2a28b379f876059dbd5417c4
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2007-07-24 03:58:50 +0000

    [互換性に影響がある変更][#YMIR-79]に対処した。（Messagesコンポーネントがymir-convention.diconからymir-component.diconに移動になった）
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@865 ce2cb714-bd0d-0410-8c66-e472ade7ad34

---
+++ ymir-core/src/test/java/org/seasar/ymir/impl/AppDiconComponentImpl.java
@@ -0,0 +1,27 @@
+package org.seasar.ymir.impl;
+
+import org.seasar.ymir.AppDiconComponent;
+import org.seasar.ymir.Messages;
+import org.seasar.ymir.Request;
+
+public class AppDiconComponentImpl implements AppDiconComponent {
+    private Messages messages_;
+
+    private Request ymirRequest_;
+
+    public Messages getMessages() {
+        return messages_;
+    }
+
+    public void setMessages(Messages messages) {
+        messages_ = messages;
+    }
+
+    public Request getYmirRequest() {
+        return ymirRequest_;
+    }
+
+    public void setYmirRequest(Request ymirRequest) {
+        ymirRequest_ = ymirRequest;
+    }
+}
---
+++ ymir-core/src/test/java/skeleton/web/HoePage.java
@@ -0,0 +1,18 @@
+package skeleton.web;
+
+import org.seasar.ymir.AppDiconComponent;
+
+public class HoePage {
+    private AppDiconComponent appDiconComponent_;
+
+    public AppDiconComponent getAppDiconComponent() {
+        return appDiconComponent_;
+    }
+
+    public void setAppDiconComponent(AppDiconComponent appDiconComponent) {
+        appDiconComponent_ = appDiconComponent;
+    }
+
+    public void _get() {
+    }
+}
