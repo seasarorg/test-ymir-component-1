d3e429b7794b012ee6e37f170fe67e9491481a6a
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2007-08-07 12:27:27 +0000

    base applicationとして設定したApplicationインスタンスがgetApplications()で返されない不具合を修正した。
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@903 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir-core/src/main/java/org/seasar/ymir/impl/ApplicationManagerImpl.java
+++ ymir-core/src/main/java/org/seasar/ymir/impl/ApplicationManagerImpl.java
@@ -5,7 +5,6 @@ import org.seasar.ymir.ApplicationManager;
 import org.seasar.framework.util.ArrayUtil;
 
 public class ApplicationManagerImpl implements ApplicationManager {
-
     private Application[] applications_ = new Application[0];
 
     private Application baseApplication_;
@@ -13,23 +12,19 @@ public class ApplicationManagerImpl implements ApplicationManager {
     private ThreadLocal<Application> application_ = new ThreadLocal<Application>();
 
     public void addApplication(Application application) {
-
         applications_ = (Application[]) ArrayUtil.add(applications_,
                 application);
     }
 
     public Application[] getApplications() {
-
         return applications_;
     }
 
     public Application getContextApplication() {
-
         return (Application) application_.get();
     }
 
     public Application findContextApplication() {
-
         Application application = getContextApplication();
         if (application == null) {
             application = baseApplication_;
@@ -38,12 +33,13 @@ public class ApplicationManagerImpl implements ApplicationManager {
     }
 
     public void setContextApplication(Application application) {
-
         application_.set(application);
     }
 
     public void setBaseApplication(Application application) {
-
         baseApplication_ = application;
+        if (application != null) {
+            addApplication(application);
+        }
     }
 }
