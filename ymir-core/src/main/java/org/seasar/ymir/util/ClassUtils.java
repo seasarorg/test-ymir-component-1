e9e99248914ca4da7cc75241b95990d8f7c51f4d
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2007-08-07 10:39:00 +0000

    Maven2でClassUtilsTestが通らなかった問題に対処した。
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@898 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir-core/src/main/java/org/seasar/ymir/util/ClassUtils.java
+++ ymir-core/src/main/java/org/seasar/ymir/util/ClassUtils.java
@@ -1,26 +1,42 @@
 package org.seasar.ymir.util;
 
 import javassist.CannotCompileException;
+import javassist.ClassClassPath;
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.NotFoundException;
 
 public class ClassUtils {
+    private static ClassPool cp_;
+
+    static {
+        cp_ = new ClassPool(null);
+        cp_.appendSystemPath();
+    }
+
     private ClassUtils() {
     }
 
     @SuppressWarnings("unchecked")
     public static <T> T newInstanceFromAbstractClass(Class<T> clazz) {
-        ClassPool cp = ClassPool.getDefault();
-        CtClass ctClass;
-        try {
-            ctClass = cp.get(clazz.getName());
-        } catch (NotFoundException ex) {
-            throw new RuntimeException("Can't happen!", ex);
+        CtClass cc;
+        synchronized (cp_) {
+            CtClass ctClass;
+            try {
+                ctClass = cp_.get(clazz.getName());
+            } catch (NotFoundException ex) {
+                // Maven2からテストするとなぜかNotFoundになってしまうので…。
+                cp_.appendClassPath(new ClassClassPath(clazz));
+                try {
+                    ctClass = cp_.get(clazz.getName());
+                } catch (NotFoundException ex1) {
+                    throw new RuntimeException("Can't happen!", ex);
+                }
+            }
+            cc = cp_.makeClass(clazz.getName() + "$$" + new Object().hashCode()
+                    + "$$", ctClass);
         }
 
-        CtClass cc = cp.makeClass(clazz.getName() + "$$"
-                + new Object().hashCode() + "$$", ctClass);
         try {
             return (T) cc.toClass().newInstance();
         } catch (InstantiationException ex) {
