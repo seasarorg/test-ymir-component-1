f01011376b9a224d54af92564dc6e66576aaad64
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2007-08-20 01:15:08 +0000

    余計なimportなどを削除した。
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@927 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir-beantable/src/main/java/org/seasar/ymir/beantable/BeantableClassHandler.java
+++ ymir-beantable/src/main/java/org/seasar/ymir/beantable/BeantableClassHandler.java
@@ -1,8 +1,5 @@
 package org.seasar.ymir.beantable;
 
-import java.sql.SQLException;
-
-import org.seasar.cms.beantable.Beantable;
 import org.seasar.framework.container.annotation.tiger.Binding;
 import org.seasar.framework.container.annotation.tiger.BindingType;
 import org.seasar.framework.log.Logger;
@@ -19,7 +16,6 @@ public class BeantableClassHandler implements ClassHandler {
     }
 
     public void processClass(String packageName, String shortClassName) {
-        Class beanClass;
         ClassLoader cl = Thread.currentThread().getContextClassLoader();
         if (cl == null) {
             cl = getClass().getClassLoader();
@@ -29,41 +25,41 @@ public class BeantableClassHandler implements ClassHandler {
             // hotdeployClassloaderでclassを読み込むようにしているため、
             // テーブルの更新処理自体はhotdeployListenerであるBeantableManagerが
             // やってくれる。
-            beanClass = Class.forName(className, true, cl);
+            Class.forName(className, true, cl);
         } catch (ClassNotFoundException ex) {
             if (logger_.isDebugEnabled()) {
                 logger_.debug("[SKIP] Class not found: " + className);
             }
             return;
         }
-//
-//        if (!manager_.isManaged(beanClass)) {
-//            return;
-//        }
-//
-//        if (logger_.isInfoEnabled()) {
-//            logger_.info("UPDATE TABLE FOR class: " + className);
-//        }
-//
-//        Beantable beanTable = manager_.newBeantable(beanClass);
-//        try {
-//            beanTable.activate();
-//        } catch (SQLException ex) {
-//            logger_.error("[SKIP] Can't activate Beantable for: " + className,
-//                    ex);
-//            return;
-//        }
-//        try {
-//            beanTable.update(false);
-//        } catch (SQLException ex) {
-//            logger_
-//                    .error("[SKIP] Can't update Beantable for: " + className,
-//                            ex);
-//            return;
-//        }
-//
-//        if (logger_.isInfoEnabled()) {
-//            logger_.info("TABLE UPDATED SUCCESSFULLY");
-//        }
+        //
+        //        if (!manager_.isManaged(beanClass)) {
+        //            return;
+        //        }
+        //
+        //        if (logger_.isInfoEnabled()) {
+        //            logger_.info("UPDATE TABLE FOR class: " + className);
+        //        }
+        //
+        //        Beantable beanTable = manager_.newBeantable(beanClass);
+        //        try {
+        //            beanTable.activate();
+        //        } catch (SQLException ex) {
+        //            logger_.error("[SKIP] Can't activate Beantable for: " + className,
+        //                    ex);
+        //            return;
+        //        }
+        //        try {
+        //            beanTable.update(false);
+        //        } catch (SQLException ex) {
+        //            logger_
+        //                    .error("[SKIP] Can't update Beantable for: " + className,
+        //                            ex);
+        //            return;
+        //        }
+        //
+        //        if (logger_.isInfoEnabled()) {
+        //            logger_.info("TABLE UPDATED SUCCESSFULLY");
+        //        }
     }
 }
