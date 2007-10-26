cf1ee95d1ea46e9b94392f0441c06de38873f804
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2007-10-26 09:21:00 +0000

    [#YMIR-144]に対処した。
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@1086 ce2cb714-bd0d-0410-8c66-e472ade7ad34

---
+++ ymir-zpt/src/main/java/org/seasar/ymir/zpt/YmirFreyjaServlet.java
@@ -0,0 +1,21 @@
+package org.seasar.ymir.zpt;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.seasar.ymir.LocaleManager;
+import org.seasar.ymir.YmirContext;
+
+import net.skirnir.freyja.webapp.FreyjaServlet;
+
+public class YmirFreyjaServlet extends FreyjaServlet {
+    private static final long serialVersionUID = 4772325478215847574L;
+
+    @Override
+    protected Locale getLocale(HttpServletRequest request) {
+        return ((LocaleManager) YmirContext.getYmir().getApplication()
+                .getS2Container().getComponent(LocaleManager.class))
+                .getLocale();
+    }
+}
