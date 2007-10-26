eb7da7aae9385f53b4bbea050c69fbf73eca5b25
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2007-10-26 11:21:08 +0000

    [#YMIR-145]に対処した。
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@1092 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir-zpt/src/main/java/org/seasar/ymir/zpt/LocalizationPathResolver.java
+++ ymir-zpt/src/main/java/org/seasar/ymir/zpt/LocalizationPathResolver.java
@@ -8,18 +8,17 @@ import org.seasar.kvasir.util.collection.PropertyReader;
 import org.seasar.ymir.Globals;
 import org.seasar.ymir.MessageNotFoundRuntimeException;
 import org.seasar.ymir.Messages;
-import org.seasar.ymir.Request;
-import org.seasar.ymir.util.MessagesUtils;
 
 import net.skirnir.freyja.TemplateContext;
 import net.skirnir.freyja.VariableResolver;
+import net.skirnir.freyja.webapp.ServletVariableResolver;
 import net.skirnir.freyja.zpt.tales.NotePathResolver;
 
 public class LocalizationPathResolver extends NotePathResolver {
     public boolean accept(TemplateContext context,
             VariableResolver varResolver, Object obj, String child) {
         return (super.accept(context, varResolver, obj, child)
-                || obj instanceof I18NPropertyReader
+                || obj instanceof Messages || obj instanceof I18NPropertyReader
                 || obj instanceof PropertyReader || obj instanceof AttributeReader);
     }
 
@@ -30,43 +29,36 @@ public class LocalizationPathResolver extends NotePathResolver {
             return resolved;
         }
 
-        if (obj instanceof I18NPropertyReader) {
-            I18NPropertyReader reader = (I18NPropertyReader) obj;
-            Locale locale = getNoteLocalizer().findLocale(context, varResolver);
+        if (obj instanceof Messages) {
+            Messages messages = (Messages) obj;
             String messageName;
             String value;
             if (child.startsWith("%")) {
                 messageName = child.substring(1);
-                String pageSpecificMessageName = MessagesUtils
-                        .getPageSpecificName(messageName, MessagesUtils
-                                .getPageName((Request) varResolver.getVariable(
-                                        context,
-                                        YmirVariableResolver.NAME_YMIRREQUEST)));
-                value = reader.getProperty(pageSpecificMessageName, locale);
-                if (value == null) {
-                    value = reader.getProperty(messageName, locale);
-                }
+                value = messages.getMessage(messageName);
             } else {
                 messageName = child;
-                String pageSpecificMessageName = MessagesUtils
-                        .getPageSpecificName(messageName, MessagesUtils
-                                .getPageName((Request) varResolver.getVariable(
-                                        context,
-                                        YmirVariableResolver.NAME_YMIRREQUEST)));
-                value = reader.getProperty(pageSpecificMessageName);
-                if (value == null) {
-                    value = reader.getProperty(messageName);
-                }
+                value = messages.getProperty(messageName);
             }
-            if (value == null && obj instanceof Messages) {
+            if (value == null) {
                 throw new MessageNotFoundRuntimeException(
                         "Message corresponding key ('" + messageName
                                 + "') does not exist in default Messages ("
                                 + Globals.MESSAGES + ")").setMessageKey(
-                        messageName).setLocale(
-                        child.startsWith("%") ? locale : new Locale(""));
+                        messageName)
+                        .setLocale(
+                                child.startsWith("%") ? findLocale(context,
+                                        varResolver) : new Locale(""));
             }
             return value;
+        } else if (obj instanceof I18NPropertyReader) {
+            I18NPropertyReader reader = (I18NPropertyReader) obj;
+            if (child.startsWith("%")) {
+                return reader.getProperty(child.substring(1), findLocale(
+                        context, varResolver));
+            } else {
+                return reader.getProperty(child);
+            }
         } else if (obj instanceof PropertyReader) {
             return ((PropertyReader) obj).getProperty(child);
         } else if (obj instanceof AttributeReader) {
@@ -74,5 +66,10 @@ public class LocalizationPathResolver extends NotePathResolver {
         }
 
         return null;
+    }
+
+    Locale findLocale(TemplateContext context, VariableResolver varResolver) {
+        return (Locale) varResolver.getVariable(context,
+                ServletVariableResolver.VAR_LOCALE);
     }
 }
