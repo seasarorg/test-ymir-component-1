a91cc142c52710b7f9618af780a3e34e6f6706a4
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2007-11-17 15:16:58 +0000

    [#YMIR-154]に対処した。
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@1148 ce2cb714-bd0d-0410-8c66-e472ade7ad34

---
+++ ymir-it/src/test/java/com/example/web/URIParameterITest1Page.java
@@ -0,0 +1,26 @@
+package com.example.web;
+
+public class URIParameterITest1Page {
+    private String category_;
+
+    private int sequence_;
+
+    public String getCategory() {
+        return category_;
+    }
+
+    public void setCategory(String category) {
+        category_ = category;
+    }
+
+    public int getSequence() {
+        return sequence_;
+    }
+
+    public void setSequence(int sequence) {
+        sequence_ = sequence;
+    }
+
+    public void _get() {
+    }
+}
---
+++ ymir-it/src/test/java/org/seasar/ymir/URIParameterITest.java
@@ -0,0 +1,36 @@
+package org.seasar.ymir;
+
+import org.seasar.ymir.test.YmirTestCase;
+
+import com.example.web.URIParameterITest1Page;
+import com.example.web.URIParameterITest2Page;
+
+public class URIParameterITest extends YmirTestCase {
+    public void test_URIに指定したパラメータがPageにDIされること() throws Exception {
+        Request request = prepareForProcessing(
+                "/URIParameterITest1/science&technology/15.html",
+                Request.METHOD_GET);
+        URIParameterITest1Page page = getComponent(URIParameterITest1Page.class);
+        processRequest(request);
+
+        assertNull(request.getParameter("category"));
+        assertNull(request.getParameter("sequence"));
+        assertEquals("science&technology", page.getCategory());
+        assertEquals(15, page.getSequence());
+    }
+
+    public void test_forwardした後はforward先のURIに指定したパラメータがPageにDIされること()
+            throws Exception {
+        Request request = prepareForProcessing(
+                "/URIParameterITest1/science&technology/15.html",
+                Request.METHOD_GET);
+        process(request);
+
+        prepareForProcessing("/URIParameterITest2/10.html", Dispatcher.FORWARD);
+        URIParameterITest2Page page = getComponent(URIParameterITest2Page.class);
+        processRequest(request);
+
+        assertNull(page.getCategory());
+        assertEquals(10, page.getSequence());
+    }
+}
