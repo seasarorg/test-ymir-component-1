6601d5f4e3b114d8a00c453ae78507c9e3099303
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2008-03-11 06:45:07 +0000

    [#YMIR-183]に対処した。
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@1234 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir-core/src/main/java/org/seasar/ymir/convention/YmirNamingConvention.java
+++ ymir-core/src/main/java/org/seasar/ymir/convention/YmirNamingConvention.java
@@ -1,22 +1,76 @@
 package org.seasar.ymir.convention;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 import org.seasar.cms.pluggable.impl.PluggableNamingConventionImpl;
+import org.seasar.framework.exception.EmptyRuntimeException;
+import org.seasar.framework.util.StringUtil;
 import org.seasar.kvasir.util.PropertyUtils;
 
 public class YmirNamingConvention extends PluggableNamingConventionImpl {
+    private static final char PACKAGE_SEPARATOR = '_';
+
+    private static final String PACKAGE_SEPARATOR_STR = "_";
+
     public static final String SUFFIX_EXCEPTIONHANDLER = "Handler";
 
     public static final String SUFFIX_CONSTRAINT = "Constraint";
 
     @Override
     public String fromComponentNameToPartOfClassName(String componentName) {
-        String partOfClassName = super
-                .fromComponentNameToPartOfClassName(componentName);
-        if (componentName.startsWith("_")) {
-            return "_" + partOfClassName;
-        } else {
-            return partOfClassName;
+        if (componentName == null) {
+            throw new EmptyRuntimeException("componentName");
         }
+        String[] names = split(componentName);
+        StringBuffer buf = new StringBuffer(50);
+        for (int i = 0; i < names.length; ++i) {
+            if (i == names.length - 1) {
+                buf.append(StringUtil.capitalize(names[i]));
+            } else {
+                buf.append(names[i]).append(".");
+            }
+        }
+        return buf.toString();
+    }
+
+    String[] split(String str) {
+        List<String> list = new ArrayList<String>();
+        int prefixLength = 0;
+        boolean separatorDetected = true;
+        for (StringTokenizer st = new StringTokenizer(str,
+                PACKAGE_SEPARATOR_STR, true); st.hasMoreTokens();) {
+            String tkn = st.nextToken();
+            if (separatorDetected) {
+                if (PACKAGE_SEPARATOR_STR.equals(tkn)) {
+                    prefixLength++;
+                } else {
+                    separatorDetected = false;
+                    if (prefixLength > 0) {
+                        StringBuilder sb = new StringBuilder(prefixLength
+                                + tkn.length());
+                        for (int i = 0; i < prefixLength; i++) {
+                            sb.append(PACKAGE_SEPARATOR);
+                        }
+                        sb.append(tkn);
+                        tkn = sb.toString();
+                        prefixLength = 0;
+                    }
+                    list.add(tkn);
+                }
+            } else {
+                separatorDetected = true;
+            }
+        }
+        if (prefixLength > 0) {
+            StringBuilder sb = new StringBuilder(prefixLength);
+            for (int i = 0; i < prefixLength; i++) {
+                sb.append(PACKAGE_SEPARATOR);
+            }
+            list.add(sb.toString());
+        }
+        return list.toArray(new String[0]);
     }
 
     @Override
--- ymir-core/src/test/java/org/seasar/ymir/convention/YmirNamingConventionTest.java
+++ ymir-core/src/test/java/org/seasar/ymir/convention/YmirNamingConventionTest.java
@@ -16,4 +16,102 @@ public class YmirNamingConventionTest extends TestCase {
         assertEquals("com.example.ignore1", actual[idx++]);
         assertEquals("com.example2.ignore", actual[idx++]);
     }
+
+    public void testFromComponentNameToClassName() throws Exception {
+        target_.addRootPackageName("com.example");
+
+        assertEquals("com.example.web.RootPage", target_
+                .fromComponentNameToClassName("rootPage"));
+        assertEquals("com.example.web._RootPage", target_
+                .fromComponentNameToClassName("_RootPage"));
+        assertEquals("com.example.web.sub.RootPage", target_
+                .fromComponentNameToClassName("sub_rootPage"));
+        assertEquals("com.example.web.sub._RootPage", target_
+                .fromComponentNameToClassName("sub__RootPage"));
+    }
+
+    public void testFromComponentNameToPartOfClassName() throws Exception {
+        target_.addRootPackageName("com.example");
+
+        assertEquals("RootPage", target_
+                .fromComponentNameToPartOfClassName("rootPage"));
+        assertEquals("_RootPage", target_
+                .fromComponentNameToPartOfClassName("_RootPage"));
+        assertEquals("sub.RootPage", target_
+                .fromComponentNameToPartOfClassName("sub_rootPage"));
+        assertEquals("sub._RootPage", target_
+                .fromComponentNameToPartOfClassName("sub__RootPage"));
+    }
+
+    public void testFromClassNameToComponentName() throws Exception {
+        target_.addRootPackageName("com.example");
+
+        assertEquals("rootPage", target_
+                .fromClassNameToComponentName("com.example.web.RootPage"));
+        assertEquals("_RootPage", target_
+                .fromClassNameToComponentName("com.example.web._RootPage"));
+        assertEquals("sub_rootPage", target_
+                .fromClassNameToComponentName("com.example.web.sub.RootPage"));
+        assertEquals("sub__RootPage", target_
+                .fromClassNameToComponentName("com.example.web.sub._RootPage"));
+    }
+
+    public void testFromClassNameToShortComponentName() throws Exception {
+        target_.addRootPackageName("com.example");
+
+        assertEquals("rootPage", target_
+                .fromClassNameToShortComponentName("com.example.web.RootPage"));
+        assertEquals("_RootPage", target_
+                .fromClassNameToShortComponentName("com.example.web._RootPage"));
+        assertEquals(
+                "rootPage",
+                target_
+                        .fromClassNameToShortComponentName("com.example.web.sub.RootPage"));
+        assertEquals(
+                "_RootPage",
+                target_
+                        .fromClassNameToShortComponentName("com.example.web.sub._RootPage"));
+    }
+
+    public void testSplit1() throws Exception {
+        String[] actual = target_.split("a");
+
+        assertEquals(1, actual.length);
+        int idx = 0;
+        assertEquals("a", actual[idx++]);
+    }
+
+    public void testSplit2() throws Exception {
+        String[] actual = target_.split("a_b");
+
+        assertEquals(2, actual.length);
+        int idx = 0;
+        assertEquals("a", actual[idx++]);
+        assertEquals("b", actual[idx++]);
+    }
+
+    public void testSplit3() throws Exception {
+        String[] actual = target_.split("_a");
+
+        assertEquals(1, actual.length);
+        int idx = 0;
+        assertEquals("_a", actual[idx++]);
+    }
+
+    public void testSplit4() throws Exception {
+        String[] actual = target_.split("a__b");
+
+        assertEquals(2, actual.length);
+        int idx = 0;
+        assertEquals("a", actual[idx++]);
+        assertEquals("_b", actual[idx++]);
+    }
+
+    public void testSplit5() throws Exception {
+        String[] actual = target_.split("a_");
+
+        assertEquals(1, actual.length);
+        int idx = 0;
+        assertEquals("a", actual[idx++]);
+    }
 }
