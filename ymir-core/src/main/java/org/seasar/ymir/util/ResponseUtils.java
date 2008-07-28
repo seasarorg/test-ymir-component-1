a6132a5514ee2e1d82c72546407b1a3d87fa64c9
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2008-07-28 11:26:10 +0000

    Cacheヘッダとしてprivateをつけるためのメソッドを追加した。
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@1298 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir-core/src/main/java/org/seasar/ymir/util/ResponseUtils.java
+++ ymir-core/src/main/java/org/seasar/ymir/util/ResponseUtils.java
@@ -37,4 +37,31 @@ public class ResponseUtils {
                 .setHeader("Cache-Control", "no-cache,no-store,must-revalidate");
         response.setDateHeader("Expires", 1);
     }
+
+    /**
+     * レスポンスがprivateにのみキャッシュされるようにするためのヘッダ情報をResponseオブジェクトに設定します。
+     * <p>これは<code>setPrivateCache(response, false)</code>と等価です。
+     * 
+     * @param response Responseオブジェクト。nullを指定した場合は何もしません。
+     */
+    public static void setPrivateCache(Response response) {
+        setPrivateCache(response, false);
+    }
+
+    /**
+     * レスポンスがprivateにのみキャッシュされるようにするためのヘッダ情報をResponseオブジェクトに設定します。
+     * 
+     * @param response Responseオブジェクト。nullを指定した場合は何もしません。
+     * @param force falseの場合、既にCache-Controlヘッダが設定されていれば何もしません。
+     * trueの場合、Cache-Controlヘッダの情報を上書きします。
+     */
+    public static void setPrivateCache(Response response, boolean force) {
+        if (response == null || !force
+                && response.containsHeader("Cache-Control")) {
+            return;
+        }
+
+        response.setHeader("Pragma", "No-cache");
+        response.setHeader("Cache-Control", "private");
+    }
 }
