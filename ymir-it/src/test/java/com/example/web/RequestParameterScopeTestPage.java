6746f323eac22d097bb988714ce2c7aee4b023cf
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2008-08-06 08:55:18 +0000

    [#YMIR-220]に対処した。
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@1353 ce2cb714-bd0d-0410-8c66-e472ade7ad34

---
+++ ymir-it/src/test/java/com/example/web/RequestParameterScopeTestPage.java
@@ -0,0 +1,75 @@
+package com.example.web;
+
+import org.seasar.ymir.annotation.In;
+import org.seasar.ymir.scope.impl.RequestParameterScope;
+
+public class RequestParameterScopeTestPage {
+    private String injectedValue1_;
+
+    private String injectedValue2_;
+
+    private String[] injectedValue3_;
+
+    private String[] injectedValue4_;
+
+    private Integer injectedValue5_;
+
+    private Integer[] injectedValue6_;
+
+    @In(RequestParameterScope.class)
+    public void setInjectedValue1(String value) {
+        injectedValue1_ = value;
+    }
+
+    @In(RequestParameterScope.class)
+    public void setInjectedValue2(String value) {
+        injectedValue2_ = value;
+    }
+
+    @In(RequestParameterScope.class)
+    public void setInjectedValue3(String[] value) {
+        injectedValue3_ = value;
+    }
+
+    @In(RequestParameterScope.class)
+    public void setInjectedValue4(String[] value) {
+        injectedValue4_ = value;
+    }
+
+    @In(RequestParameterScope.class)
+    public void setInjectedValue5(Integer injectedValue5) {
+        injectedValue5_ = injectedValue5;
+    }
+
+    @In(RequestParameterScope.class)
+    public void setInjectedValue6(Integer[] injectedValue6) {
+        injectedValue6_ = injectedValue6;
+    }
+
+    public String getInjectedValue1() {
+        return injectedValue1_;
+    }
+
+    public String getInjectedValue2() {
+        return injectedValue2_;
+    }
+
+    public String[] getInjectedValue3() {
+        return injectedValue3_;
+    }
+
+    public String[] getInjectedValue4() {
+        return injectedValue4_;
+    }
+
+    public Integer getInjectedValue5() {
+        return injectedValue5_;
+    }
+
+    public Integer[] getInjectedValue6() {
+        return injectedValue6_;
+    }
+
+    public void _get() {
+    }
+}
---
+++ ymir-it/src/test/java/org/seasar/ymir/scope/impl/RequestParameterScopeITest.java
@@ -0,0 +1,37 @@
+package org.seasar.ymir.scope.impl;
+
+import org.seasar.ymir.Request;
+import org.seasar.ymir.RequestProcessor;
+import org.seasar.ymir.test.PageTestCase;
+
+import com.example.web.RequestParameterScopeTestPage;
+
+public class RequestParameterScopeITest extends
+        PageTestCase<RequestParameterScopeTestPage> {
+    @Override
+    protected Class<RequestParameterScopeTestPage> getPageClass() {
+        return RequestParameterScopeTestPage.class;
+    }
+
+    public void test() throws Exception {
+        Request request = prepareForProcessing(
+                "/requestParameterScopeTest.html",
+                Request.METHOD_GET,
+                "injectedValue1=1&injectedValue2=2&injectedValue2=2&injectedValue3=3&injectedValue4=4&injectedValue4=4&injectedValue5=5&injectedValue6=6&injectedValue6=6");
+        processRequest(request);
+
+        RequestParameterScopeTestPage page = (RequestParameterScopeTestPage) request
+                .getAttribute(RequestProcessor.ATTR_SELF);
+        assertEquals("1", page.getInjectedValue1());
+        assertEquals("2", page.getInjectedValue2());
+        assertEquals(1, page.getInjectedValue3().length);
+        assertEquals("3", page.getInjectedValue3()[0]);
+        assertEquals(2, page.getInjectedValue4().length);
+        assertEquals("4", page.getInjectedValue4()[0]);
+        assertEquals("4", page.getInjectedValue4()[1]);
+        assertEquals(Integer.valueOf(5), page.getInjectedValue5());
+        assertEquals(2, page.getInjectedValue6().length);
+        assertEquals(Integer.valueOf(6), page.getInjectedValue6()[0]);
+        assertEquals(Integer.valueOf(6), page.getInjectedValue6()[1]);
+    }
+}
