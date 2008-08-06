f014b296d8aa1439b9b32f20f381aa0b617629ff
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2008-08-06 08:19:07 +0000

    [#YMIR-219]に対処した。
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@1348 ce2cb714-bd0d-0410-8c66-e472ade7ad34

---
+++ ymir-core/src/main/java/org/seasar/ymir/scope/impl/ResponseHeaderScope.java
@@ -0,0 +1,86 @@
+package org.seasar.ymir.scope.impl;
+
+import java.lang.reflect.Array;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.seasar.framework.container.annotation.tiger.Binding;
+import org.seasar.framework.container.annotation.tiger.BindingType;
+import org.seasar.ymir.TypeConversionManager;
+
+/**
+ * レスポンスヘッダに値を設定するための仮想的なスコープを表すクラスです。
+ * <p>{@link #setAttribute(String, Object)}に指定された値の型が{@link Date}型の場合、
+ * {@link HttpServletResponse#setDateHeader(String, long)}を使ってヘッダが設定されます。
+ * 値の型が{@link Number}型の場合、
+ * {@link HttpServletResponse#setIntHeader(String, int)}を使ってヘッダが設定されます。
+ * それ以外の場合は
+ * {@link TypeConversionManager}を使って値を文字列に変換した上で
+ * {@link HttpServletResponse#setHeader(String, String)}を使ってヘッダが設定されます。
+ * </p>
+ * <p>{@link #setAttribute(String, Object)}に指定された値が配列型の場合は
+ * <code>HttpServletResponse#addHeader()</code>を使って全ての値がヘッダとして設定されます。
+ * そうでない場合は
+ * <code>HttpServletResponse#setHeader()</code>を使ってヘッダが設定されます。
+ * </p>
+ * <p><b>同期化：</b>
+ * このクラスはスレッドセーフです。
+ * </p>
+ * 
+ * @author YOKOTA Takehiko
+ */
+public class ResponseHeaderScope extends AbstractServletScope {
+    private TypeConversionManager typeConversionManager_;
+
+    @Binding(bindingType = BindingType.MUST)
+    public void setTypeConversionManager(
+            TypeConversionManager typeConversionManager) {
+        typeConversionManager_ = typeConversionManager;
+    }
+
+    public Object getAttribute(String name) {
+        throw new UnsupportedOperationException(
+                "Can't get response header: name=" + name);
+    }
+
+    public void setAttribute(String name, Object value) {
+        HttpServletResponse response = getResponse();
+        if (value == null) {
+            ;
+        } else if (value.getClass().isArray()) {
+            int length = Array.getLength(value);
+            for (int i = 0; i < length; i++) {
+                addHeader(response, name, Array.get(value, i));
+            }
+        } else {
+            setHeader(response, name, value);
+        }
+    }
+
+    protected void addHeader(HttpServletResponse response, String name,
+            Object value) {
+        Class<?> clazz = value.getClass();
+        if (Date.class.isAssignableFrom(clazz)) {
+            response.addDateHeader(name, ((Date) value).getTime());
+        } else if (Number.class.isAssignableFrom(clazz)) {
+            response.addIntHeader(name, ((Number) value).intValue());
+        } else {
+            response.addHeader(name, typeConversionManager_.convert(value,
+                    String.class));
+        }
+    }
+
+    protected void setHeader(HttpServletResponse response, String name,
+            Object value) {
+        Class<?> clazz = value.getClass();
+        if (Date.class.isAssignableFrom(clazz)) {
+            response.setDateHeader(name, ((Date) value).getTime());
+        } else if (Number.class.isAssignableFrom(clazz)) {
+            response.setIntHeader(name, ((Number) value).intValue());
+        } else {
+            response.setHeader(name, typeConversionManager_.convert(value,
+                    String.class));
+        }
+    }
+}
