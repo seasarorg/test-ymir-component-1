7bbf92a82f630c02fc4279550b06dddd43ea4379
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2008-08-06 09:33:44 +0000

    正しくRequestHeaderをとることができなかった問題に対処した。
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@1358 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir-core/src/main/java/org/seasar/ymir/scope/impl/RequestHeaderScope.java
+++ ymir-core/src/main/java/org/seasar/ymir/scope/impl/RequestHeaderScope.java
@@ -1,5 +1,9 @@
 package org.seasar.ymir.scope.impl;
 
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
 /**
  * リクエストヘッダをインジェクトするための仮想的なスコープを表すクラスです。
  * <p><b>同期化：</b>
@@ -10,7 +14,12 @@ package org.seasar.ymir.scope.impl;
  */
 public class RequestHeaderScope extends AbstractServletScope {
     public Object getAttribute(String name) {
-        return getRequest().getHeaders(name);
+        List<String> list = new ArrayList<String>();
+        for (Enumeration<?> enm = getRequest().getHeaders(name); enm
+                .hasMoreElements();) {
+            list.add((String) enm.nextElement());
+        }
+        return list.toArray(new String[0]);
     }
 
     public void setAttribute(String name, Object value) {
