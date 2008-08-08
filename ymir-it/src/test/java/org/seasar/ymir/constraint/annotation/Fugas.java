118c8532f0f35e779f3855f02951f731f995c2e6
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2008-08-08 05:51:31 +0000

    [#YMIR-222]に対処した。
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@1365 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir-it/src/test/java/org/seasar/ymir/constraint/annotation/Fugas.java
+++ ymir-it/src/test/java/org/seasar/ymir/constraint/annotation/Fugas.java
@@ -5,9 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.seasar.ymir.annotation.Collection;
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target( { ElementType.TYPE, ElementType.METHOD })
-@ConstraintsAnnotation
+@Collection
 public @interface Fugas {
     Fuga[] value();
 }
