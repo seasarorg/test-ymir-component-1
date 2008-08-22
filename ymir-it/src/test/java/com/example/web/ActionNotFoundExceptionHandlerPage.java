15cb8ba9b224b047da940260c43d53aa6e26eb6c
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2008-08-22 16:11:10 +0000

    [#YMIR-227]に対処した。
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@1423 ce2cb714-bd0d-0410-8c66-e472ade7ad34

---
+++ ymir-it/src/test/java/com/example/web/ActionNotFoundExceptionHandlerPage.java
@@ -0,0 +1,9 @@
+package com.example.web;
+
+public class ActionNotFoundExceptionHandlerPage {
+    public void _get() {
+    }
+
+    public void _post() {
+    }
+}
---
+++ ymir-it/src/test/java/org/seasar/ymir/handler/impl/ActionNotFoundExceptionHandlerITest.java
@@ -0,0 +1,23 @@
+package org.seasar.ymir.handler.impl;
+
+import org.seasar.ymir.Request;
+import org.seasar.ymir.test.PageTestCase;
+
+import com.example.web.ActionNotFoundExceptionHandlerPage;
+
+public class ActionNotFoundExceptionHandlerITest extends
+        PageTestCase<ActionNotFoundExceptionHandlerPage> {
+    @Override
+    protected Class<ActionNotFoundExceptionHandlerPage> getPageClass() {
+        return ActionNotFoundExceptionHandlerPage.class;
+    }
+
+    public void test_アクションが見つからなかった時に405を返すこと() throws Exception {
+        Request request = prepareForProcessing(
+                "/actionNotFoundExceptionHandler.html", Request.METHOD_TRACE);
+        process(request);
+
+        assertEquals(405, getHttpServletResponse().getStatus());
+        assertEquals("GET,POST", getHttpServletResponse().getHeader("Allow"));
+    }
+}
