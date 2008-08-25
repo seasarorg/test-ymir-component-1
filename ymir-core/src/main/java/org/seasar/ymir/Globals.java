7c09af53c592b112b4d4e39ba19407244a50c2cb
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2008-08-25 05:34:40 +0000

    コメントを追加した。
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@1439 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir-core/src/main/java/org/seasar/ymir/Globals.java
+++ ymir-core/src/main/java/org/seasar/ymir/Globals.java
@@ -43,6 +43,7 @@ public interface Globals {
      * 明示的に{@link RequestParameter}アノテーションが指定されているSetter
      * にだけインジェクトするかどうかを指定するためのアプリケーションプロパティのキーです。
      */
+    // TODO ymir-1.0.xでは廃止する。
     String APPKEY_CORE_REQUESTPARAMETER_STRICTINJECTION = APPKEYPREFIX_CORE
             + "requestParameter.strictInjection";
 }
