56381c2f7b2c30f6b48f379c1af9edfa1288a40d
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2008-09-02 01:35:49 +0000

    Javadocを追記した。
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@1457 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir-core/src/main/java/org/seasar/ymir/creator/PageCreator.java
+++ ymir-core/src/main/java/org/seasar/ymir/creator/PageCreator.java
@@ -2,6 +2,8 @@ package org.seasar.ymir.creator;
 
 import org.seasar.framework.convention.NamingConvention;
 
+// TODO [YMIR-1.0] PageコンポーネントにはS2以外の機構（Scope）もオブジェクトをDIするため、混乱しないように
+// autoBindingをNONEにする。S2からDIしたい場合はBindingを明示的に書くか、@Injectを使うようにする。
 public class PageCreator extends
         org.seasar.framework.container.creator.PageCreator {
 
