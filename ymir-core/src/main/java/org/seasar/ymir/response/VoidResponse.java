2aecc835c39b8a15793cbea4b7455da4a95348e9
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2008-09-10 03:29:50 +0000

    VoidResponseのmutater呼び出しは無視するようにした。
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@1493 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir-core/src/main/java/org/seasar/ymir/response/VoidResponse.java
+++ ymir-core/src/main/java/org/seasar/ymir/response/VoidResponse.java
@@ -28,61 +28,49 @@ public class VoidResponse extends ResponseBase {
 
     @Override
     public void setContentType(String contentType) {
-        throw new UnsupportedOperationException();
     }
 
     @Override
     public void setDateHeader(String name, long value) {
-        throw new UnsupportedOperationException();
     }
 
     @Override
     public void setHeader(String name, String value) {
-        throw new UnsupportedOperationException();
     }
 
     @Override
     public void setInputStream(InputStream is) {
-        throw new UnsupportedOperationException();
     }
 
     @Override
     public void setIntHeader(String name, int value) {
-        throw new UnsupportedOperationException();
     }
 
     @Override
     public void setPath(String path) {
-        throw new UnsupportedOperationException();
     }
 
     @Override
     public void setStatus(int status) {
-        throw new UnsupportedOperationException();
     }
 
     @Override
     public void setSubordinate(boolean subordinate) {
-        throw new UnsupportedOperationException();
     }
 
     @Override
     public void setType(ResponseType type) {
-        throw new UnsupportedOperationException();
     }
 
     @Override
     public void addDateHeader(String name, long value) {
-        throw new UnsupportedOperationException();
     }
 
     @Override
     public void addHeader(String name, String value) {
-        throw new UnsupportedOperationException();
     }
 
     @Override
     public void addIntHeader(String name, int value) {
-        throw new UnsupportedOperationException();
     }
 }
