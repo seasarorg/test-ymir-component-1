fbd108e8b1c3284e9ec4e7be19f8338b5ee79358
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2008-09-10 03:28:38 +0000

    VoidResponseをimmutableかつsingletonにした。
    git-svn-id: https://www.seasar.org/svn/ymir/trunk@1492 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir-core/src/main/java/org/seasar/ymir/response/VoidResponse.java
+++ ymir-core/src/main/java/org/seasar/ymir/response/VoidResponse.java
@@ -1,11 +1,13 @@
 package org.seasar.ymir.response;
 
+import java.io.InputStream;
+
 import org.seasar.ymir.ResponseType;
 
 /**
  * フレームワークにレスポンスを出力して欲しくないことを表すResponseオブジェクトです。
  * <p><b>同期化：</b>
- * このクラスはスレッドセーフではありません。
+ * このクラスはスレッドセーフです。
  * </p>
  * 
  * @author YOKOTA Takehiko
@@ -13,11 +15,74 @@ import org.seasar.ymir.ResponseType;
 public class VoidResponse extends ResponseBase {
     public static final VoidResponse INSTANCE = new VoidResponse();
 
+    private VoidResponse() {
+    }
+
     public ResponseType getType() {
         return ResponseType.VOID;
     }
 
     public boolean isSubordinate() {
         return false;
+    }
+
+    @Override
+    public void setContentType(String contentType) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setDateHeader(String name, long value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setHeader(String name, String value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setInputStream(InputStream is) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setIntHeader(String name, int value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setPath(String path) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setStatus(int status) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setSubordinate(boolean subordinate) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setType(ResponseType type) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addDateHeader(String name, long value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addHeader(String name, String value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addIntHeader(String name, int value) {
+        throw new UnsupportedOperationException();
     }
 }
