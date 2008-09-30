d9dd9edcb0b9a6555d2ffcde6554ec20a9ba93b2
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2008-09-30 06:45:30 +0000

    [#YMIR-255]に着手中。
    git-svn-id: https://www.seasar.org/svn/ymir/component/trunk@1697 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir-testing/src/main/java/org/seasar/ymir/PopulationMetaData.java
+++
@@ -1,9 +0,0 @@
-package org.seasar.ymir;
-
-import org.seasar.ymir.scope.Scope;
-
-public interface PopulationMetaData {
-    Scope getScope();
-
-    boolean isProtected(String name, String actionName);
-}
--- ymir-testing/src/main/java/org/seasar/ymir/test/mock/servlet/MockHttpServletResponse.java
+++
@@ -1,6 +0,0 @@
-package org.seasar.ymir.test.mock.servlet;
-
-public interface MockHttpServletResponse extends
-        org.seasar.framework.mock.servlet.MockHttpServletResponse {
-    String getRedirectPath();
-}
