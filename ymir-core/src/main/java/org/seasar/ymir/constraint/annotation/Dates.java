a0baa3eeb3ad610a0c04c095743f79eb7bdeec30
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2008-10-01 18:47:49 +0000

    Datesアノテーションを追加した。
    git-svn-id: https://www.seasar.org/svn/ymir/component/trunk@1765 ce2cb714-bd0d-0410-8c66-e472ade7ad34

---
+++ ymir-core/src/main/java/org/seasar/ymir/constraint/annotation/Dates.java
@@ -0,0 +1,25 @@
+package org.seasar.ymir.constraint.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.seasar.ymir.annotation.Collection;
+
+/**
+ * {@link Date}アノテーションを複数指定するためのアノテーションです。
+ * 
+ * @author YOKOTA Takehiko
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target( { ElementType.TYPE, ElementType.METHOD })
+@Collection
+public @interface Dates {
+    /**
+     * {@link Date}アノテーションです。
+     * 
+     * @return {@link Date}アノテーション。
+     */
+    Date[] value();
+}
