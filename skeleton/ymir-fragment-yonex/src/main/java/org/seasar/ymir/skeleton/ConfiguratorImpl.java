384cb3b979639cf1df7a19be0a95776f8d3a584c
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2008-12-16 18:24:58 +0000

    vili-apiのバージョンを0.0.3-3-SNAPSHOTにした。
    git-svn-id: https://www.seasar.org/svn/ymir/component/trunk@2125 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- skeleton/ymir-fragment-yonex/src/main/java/org/seasar/ymir/skeleton/ConfiguratorImpl.java
+++ skeleton/ymir-fragment-yonex/src/main/java/org/seasar/ymir/skeleton/ConfiguratorImpl.java
@@ -1,17 +1,6 @@
 package org.seasar.ymir.skeleton;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.seasar.ymir.vili.AbstractConfigurator;
 
-import org.seasar.ymir.vili.Configurator;
-import org.seasar.ymir.vili.ViliBehavior;
-import org.seasar.ymir.vili.ViliProjectPreferences;
-
-public class ConfiguratorImpl implements Configurator {
-    public Map<String, Object> createAdditionalParameters(
-            ViliBehavior behavior, ViliProjectPreferences preferences,
-            Map<String, Object> artifactParameters) {
-        Map<String, Object> parameters = new HashMap<String, Object>();
-        return parameters;
-    }
+public class ConfiguratorImpl extends AbstractConfigurator {
 }
