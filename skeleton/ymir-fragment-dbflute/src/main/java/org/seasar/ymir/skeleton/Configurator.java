743327747e95d1772d79e0b315b0c90dd94262e4
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2008-12-23 05:09:55 +0000

    ・サンプルのreplace-schema.sqlをoracle-exampleのものに置き換えた。 ・新規時に上書き用のパラメータが設定画面に表示されないようにした。
    git-svn-id: https://www.seasar.org/svn/ymir/component/trunk@2219 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- skeleton/ymir-fragment-dbflute/src/main/java/org/seasar/ymir/skeleton/Configurator.java
+++ skeleton/ymir-fragment-dbflute/src/main/java/org/seasar/ymir/skeleton/Configurator.java
@@ -9,6 +9,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.SubProgressMonitor;
+import org.seasar.kvasir.util.PropertyUtils;
 import org.seasar.ymir.vili.AbstractConfigurator;
 import org.seasar.ymir.vili.InclusionType;
 import org.seasar.ymir.vili.ViliBehavior;
@@ -31,7 +32,23 @@ public class Configurator extends AbstractConfigurator {
     private static final String PATH_PROJECT_SH = PATHPREFIX_DBFLUTE
             + "_project.sh";
 
+    private static final String PATH_MYDBFLUTE = "mydbflute";
+
     private boolean updateBatFiles;
+
+    @Override
+    public void start(IProject project, ViliBehavior behavior,
+            ViliProjectPreferences preferences) {
+        if (!oldVersionExists(project)) {
+            behavior.getProperties().setProperty(
+                    ViliBehavior.TEMPLATE_PARAMETERS, "isDeleteOldTableClass");
+            behavior.notifyPropertiesChanged();
+        }
+    }
+
+    private boolean oldVersionExists(IProject project) {
+        return project != null && project.getFolder(PATH_MYDBFLUTE).exists();
+    }
 
     @Override
     public void processBeforeExpanding(IProject project, ViliBehavior behavior,
@@ -41,7 +58,7 @@ public class Configurator extends AbstractConfigurator {
         try {
             Boolean isDeleteOldVersion = (Boolean) parameters
                     .get(KEY_ISDELETEOLDVERSION);
-            if (isDeleteOldVersion.booleanValue()) {
+            if (PropertyUtils.valueOf(isDeleteOldVersion, false)) {
                 try {
                     deleteOldVersion(project,
                             new SubProgressMonitor(monitor, 1));
@@ -52,8 +69,8 @@ public class Configurator extends AbstractConfigurator {
                 monitor.worked(1);
             }
 
-            updateBatFiles = ((Boolean) parameters.get(KEY_UPDATEBATCHFILES))
-                    .booleanValue();
+            updateBatFiles = PropertyUtils.valueOf((Boolean) parameters
+                    .get(KEY_UPDATEBATCHFILES), false);
         } finally {
             monitor.done();
         }
@@ -84,9 +101,15 @@ public class Configurator extends AbstractConfigurator {
     public InclusionType shouldExpand(String path, String resolvedPath,
             IProject project, ViliBehavior behavior,
             ViliProjectPreferences preferences, Map<String, Object> parameters) {
+        if (!project.getFile(resolvedPath).exists()
+                && !project.getFolder(resolvedPath).exists()) {
+            return InclusionType.UNDEFINED;
+        }
+
         if (path.equals(PATH_PROJECT_BAT) || path.equals(PATH_PROJECT_SH)) {
             return InclusionType.INCLUDED;
         }
+
         if (path.startsWith(PATHPREFIX_DBFLUTE)) {
             if (updateBatFiles
                     && (path.endsWith(SUFFIX_BAT) || path.endsWith(SUFFIX_SH))) {
