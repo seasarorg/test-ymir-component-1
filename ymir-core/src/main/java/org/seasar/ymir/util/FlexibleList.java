5448765227aa49c78d0243a0aa5b0a6f9882e6dc
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2009-02-19 11:46:55 +0000

    名前に添え字がついているリクエストパラメータを受けるために便利なListクラスを追加した。
    git-svn-id: https://www.seasar.org/svn/ymir/component/trunk@2818 ce2cb714-bd0d-0410-8c66-e472ade7ad34

---
+++ ymir-core/src/main/java/org/seasar/ymir/util/FlexibleList.java
@@ -0,0 +1,60 @@
+package org.seasar.ymir.util;
+
+import java.util.AbstractList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.RandomAccess;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.Map.Entry;
+
+/**
+ * 指定されたインデックスに従って自動的に大きさを拡張するList実装です。
+ * 
+ * @since 1.0.2
+ */
+public class FlexibleList<E> extends AbstractList<E> implements List<E>,
+        RandomAccess {
+    private SortedMap<Integer, E> map = new TreeMap<Integer, E>();
+
+    public void add(int index, E element) {
+        if (map.isEmpty()) {
+            map.put(index, element);
+        } else {
+            SortedMap<Integer, E> m = new TreeMap<Integer, E>();
+            for (Iterator<Map.Entry<Integer, E>> itr = map.tailMap(index)
+                    .entrySet().iterator(); itr.hasNext();) {
+                Entry<Integer, E> entry = itr.next();
+                m.put(entry.getKey().intValue() + 1, entry.getValue());
+                itr.remove();
+            }
+
+            map.put(index, element);
+
+            for (Map.Entry<Integer, E> entry : m.entrySet()) {
+                map.put(entry.getKey(), entry.getValue());
+            }
+        }
+    }
+
+    public E get(int index) {
+        return map.get(index);
+    }
+
+    public E remove(int index) {
+        return map.remove(index);
+    }
+
+    public E set(int index, E element) {
+        return map.put(index, element);
+    }
+
+    public int size() {
+        if (map.isEmpty()) {
+            return 0;
+        } else {
+            return map.lastKey().intValue() + 1;
+        }
+    }
+}
