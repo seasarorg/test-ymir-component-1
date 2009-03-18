4c401fb665b0cc2e7fe511ea270cc8fbdf0dff24
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2009-03-18 11:57:24 +0000

    Ymir標準functionを整備した。
    git-svn-id: https://www.seasar.org/svn/ymir/component/trunk@3012 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir-zpt/src/main/java/org/seasar/ymir/zpt/YmirFunction.java
+++ ymir-zpt/src/main/java/org/seasar/ymir/zpt/YmirFunction.java
@@ -1,5 +1,9 @@
 package org.seasar.ymir.zpt;
 
+import org.seasar.ymir.RequestProcessor;
+import org.seasar.ymir.message.Notes;
+
+import net.skirnir.freyja.zpt.Default;
 import net.skirnir.freyja.zpt.tales.Function;
 import net.skirnir.freyja.zpt.tales.JavaTypePrefixHandler.InternalFunction;
 
@@ -15,7 +19,44 @@ public class YmirFunction {
                 .path(PREFIX_PARAM_SELF + name));
     }
 
+    public static boolean existsNotes() {
+        Notes notes = (Notes) InternalFunction
+                .resolve(RequestProcessor.ATTR_NOTES);
+        if (notes == null) {
+            return false;
+        }
+        return !notes.isEmpty();
+    }
+
+    public static boolean existsNotes(String category) {
+        Notes notes = (Notes) InternalFunction
+                .resolve(RequestProcessor.ATTR_NOTES);
+        if (notes == null) {
+            return false;
+        }
+        return notes.contains(category);
+    }
+
     public static boolean peq(String name, Object value) {
         return Function.eq(paramSelf(name), value);
+    }
+
+    public static Object nvl(Object classWhenNoted) {
+        return nvl(classWhenNoted, Default.instance);
+    }
+
+    public static Object nvl(Object classWhenNoted,
+            Object classWhenNotNoted) {
+        return existsNotes() ? classWhenNoted : classWhenNotNoted;
+    }
+
+    public static Object pnvl(String paramName,
+            Object classWhenNoted) {
+        return pnvl(paramName, classWhenNoted, Default.instance);
+    }
+
+    public static Object pnvl(String paramName,
+            Object classWhenNoted, Object classWhenNotNoted) {
+        return existsNotes(paramName) ? classWhenNoted : classWhenNotNoted;
     }
 }
