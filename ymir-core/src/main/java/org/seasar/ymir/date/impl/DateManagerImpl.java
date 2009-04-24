fd3956b4b4ddd1b424e5fdef43754c07f691a42f
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2009-04-24 02:02:58 +0000

    DateManagerはタイムゾーンに関する考慮がなされていなかったが、タイムゾーンを考慮するようにした。
    git-svn-id: https://www.seasar.org/svn/ymir/component/trunk@3249 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir-core/src/main/java/org/seasar/ymir/date/impl/DateManagerImpl.java
+++ ymir-core/src/main/java/org/seasar/ymir/date/impl/DateManagerImpl.java
@@ -3,14 +3,26 @@ package org.seasar.ymir.date.impl;
 import java.util.Calendar;
 import java.util.Date;
 
+import org.seasar.framework.container.annotation.tiger.Binding;
+import org.seasar.framework.container.annotation.tiger.BindingType;
+import org.seasar.ymir.annotation.ForTesting;
 import org.seasar.ymir.date.DateManager;
+import org.seasar.ymir.locale.LocaleManager;
 
 public class DateManagerImpl implements DateManager {
+    private LocaleManager localeManager_;
+
     private Date date_;
+
+    @Binding(bindingType = BindingType.MUST)
+    public void setLocaleManager(LocaleManager localeManager) {
+        localeManager_ = localeManager;
+    }
 
     public Calendar getCalendar() {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(getDate());
+        calendar.setTimeZone(localeManager_.getTimeZone());
         return calendar;
     }
 
@@ -26,6 +38,7 @@ public class DateManagerImpl implements DateManager {
         }
     }
 
+    @ForTesting
     public void setDate(Date date) {
         date_ = date;
     }
