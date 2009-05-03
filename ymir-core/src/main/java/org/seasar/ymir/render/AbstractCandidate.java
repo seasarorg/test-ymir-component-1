5eaa9dfcfb52ee5876fd7522aada77ee823f3c0f
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2009-05-03 05:21:55 +0000

    toString()を実装した。
    git-svn-id: https://www.seasar.org/svn/ymir/component/trunk@3291 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir-core/src/main/java/org/seasar/ymir/render/AbstractCandidate.java
+++ ymir-core/src/main/java/org/seasar/ymir/render/AbstractCandidate.java
@@ -1,12 +1,19 @@
 package org.seasar.ymir.render;
 
-public abstract class AbstractCandidate implements Candidate {
-    protected String value_;
+import java.lang.reflect.Array;
 
+public abstract class AbstractCandidate implements Candidate {
     protected boolean selected_;
 
-    public String getValue() {
-        return value_;
+    protected String value_;
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder().append('(');
+        sb.append("selected=").append(selected_).append(", ");
+        sb.append("value=").append(value_);
+        sb.append(')');
+        return sb.toString();
     }
 
     public boolean isSelected() {
@@ -15,6 +22,10 @@ public abstract class AbstractCandidate implements Candidate {
 
     public void setSelected(boolean selected) {
         selected_ = selected;
+    }
+
+    public String getValue() {
+        return value_;
     }
 
     public void setValue(String value) {
