8513e0d20c4e835a672c00758ecdbda47c647666
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2009-05-10 13:26:52 +0000

    返り値がvoidのアクションについて、HttpServletResponseがcommitされている場合はpassthroughでなくvoidResponseを返すようにした。
    git-svn-id: https://www.seasar.org/svn/ymir/component/trunk@3325 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir-core/src/main/java/org/seasar/ymir/response/constructor/impl/VoidResponseConstructor.java
+++ ymir-core/src/main/java/org/seasar/ymir/response/constructor/impl/VoidResponseConstructor.java
@@ -1,15 +1,37 @@
 package org.seasar.ymir.response.constructor.impl;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.seasar.framework.container.S2Container;
+import org.seasar.framework.container.annotation.tiger.Binding;
+import org.seasar.framework.container.annotation.tiger.BindingType;
 import org.seasar.ymir.Response;
 import org.seasar.ymir.response.PassthroughResponse;
+import org.seasar.ymir.response.VoidResponse;
 import org.seasar.ymir.response.constructor.ResponseConstructor;
 
 public class VoidResponseConstructor implements ResponseConstructor<Void> {
+    private S2Container container_;
+
+    @Binding(bindingType = BindingType.MUST)
+    public void setContainer(S2Container container) {
+        container_ = container;
+    }
+
     public Class<Void> getTargetClass() {
         return Void.TYPE;
     }
 
     public Response constructResponse(Object page, Void returnValue) {
-        return new PassthroughResponse();
+        if (getHttpServletResponse().isCommitted()) {
+            return VoidResponse.INSTANCE;
+        } else {
+            return new PassthroughResponse();
+        }
+    }
+
+    private HttpServletResponse getHttpServletResponse() {
+        return (HttpServletResponse) container_
+                .getComponent(HttpServletResponse.class);
     }
 }
