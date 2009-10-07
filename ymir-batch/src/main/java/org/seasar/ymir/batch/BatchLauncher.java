54cc5f8248c62ab3b9964f17666b762cc635c268
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2009-10-07 03:35:03 +0000

    servletContext.getRealPath("")が正しいディレクトリを指していなかった問題に対処した。
    git-svn-id: https://www.seasar.org/svn/ymir/component/trunk@3548 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir-batch/src/main/java/org/seasar/ymir/batch/BatchLauncher.java
+++ ymir-batch/src/main/java/org/seasar/ymir/batch/BatchLauncher.java
@@ -1,11 +1,15 @@
 package org.seasar.ymir.batch;
 
+import java.io.File;
+import java.net.URL;
+
 import javax.servlet.ServletContextEvent;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.seasar.framework.container.S2Container;
 import org.seasar.framework.container.factory.SingletonS2ContainerFactory;
+import org.seasar.framework.util.JarFileUtil;
 import org.seasar.framework.util.ResourceUtil;
 import org.seasar.kvasir.util.io.impl.FileResource;
 import org.seasar.ymir.mock.servlet.MockServletContext;
@@ -32,8 +36,7 @@ public class BatchLauncher implements Batch {
 
     public final boolean init(String[] args) throws Exception {
         servletContext = new MockServletContextImpl("/");
-        servletContext.setRoot(new FileResource(ResourceUtil.getBuildDir(Bootstrap.class).getParentFile()
-                .getParentFile()));
+        servletContext.setRoot(new FileResource(getBuildDir(Bootstrap.class).getParentFile().getParentFile()));
         servletContext.setInitParameter(YmirListener.CONFIG_PATH_KEY, "ymir.dicon");
 
         listener = new YmirListener();
@@ -60,5 +63,26 @@ public class BatchLauncher implements Batch {
 
     public S2Container getContainer() {
         return container;
+    }
+
+    private File getBuildDir(Class<?> clazz) {
+        return getBuildDir(ResourceUtil.getResourcePath(clazz));
+    }
+
+    // S2.4.20のResourceUtil.getBuildDir(String)はjarのパスを与えた
+    // 場合にjar自身を差すFileオブジェクトを返してしまう不具合があるため
+    // 自前で実装している。
+    private File getBuildDir(String path) {
+        File dir = null;
+        URL url = ResourceUtil.getResource(path);
+        if ("file".equals(url.getProtocol())) {
+            int num = path.split("/").length;
+            dir = new File(ResourceUtil.getFileName(url));
+            for (int i = 0; i < num; ++i, dir = dir.getParentFile()) {
+            }
+        } else {
+            dir = new File(JarFileUtil.toJarFilePath(url)).getParentFile();
+        }
+        return dir;
     }
 }
