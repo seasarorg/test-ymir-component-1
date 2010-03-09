bd993e1f9955af65c7d0e64502e06791a986d62d
skirnir <skirnir@ce2cb714-bd0d-0410-8c66-e472ade7ad34>
2010-03-09 08:29:15 +0000

    webrootを指定するパラメータの名前を変更した。
    git-svn-id: https://www.seasar.org/svn/ymir/component/trunk@3764 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir-scaffold/src/main/java/org/seasar/ymir/scaffold/zpt/ScaffoldingYmirFreyjaServlet.java
+++ ymir-scaffold/src/main/java/org/seasar/ymir/scaffold/zpt/ScaffoldingYmirFreyjaServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.seasar.ymir.YmirContext;
 import org.seasar.ymir.locale.LocaleManager;
+import org.seasar.ymir.zpt.JavaResourceTemplateSet;
 import org.seasar.ymir.zpt.YmirFreyjaServlet;
 
 import net.skirnir.freyja.TemplateSet;
@@ -15,29 +16,45 @@ import net.skirnir.freyja.impl.CascadingTemplateSet;
 public class ScaffoldingYmirFreyjaServlet extends YmirFreyjaServlet {
     private static final long serialVersionUID = 1L;
 
-    private static final String INITPARAM_ROOTPACKAGEPATH = "rootPackagePath";
+    private static final String INITPARAM_WEBROOTPACKAGEPATH = "webrootPackagePath";
+
+    private static final String INITPARAM_ZPTROOTPACKAGEPATH = "zptrootPackagePath";
 
     private static final String INITPARAM_RESOURCEENCODING = "resourceEncoding";
 
-    private static final String DEFAULT_ROOTPACKAGEPATH = "org/seasar/ymir/zptroot";
+    private static final String DEFAULT_WEBROOTPACKAGEPATH = "org/seasar/ymir/webroot";
+
+    private static final String DEFAULT_ZPTROOTPACKAGEPATH = "org/seasar/ymir/zptroot";
 
     private static final String DEFAULT_RESOURCEENCODING = "UTF-8";
 
-    private String rootPackagePath_;
+    private String webrootPackagePath_;
+
+    private String zptrootPackagePath_;
 
     private String resourceEncoding_;
 
     @Override
     public void init() throws ServletException {
-        String rootPackagePath = getInitParameter(INITPARAM_ROOTPACKAGEPATH);
-        if (rootPackagePath == null) {
-            rootPackagePath = DEFAULT_ROOTPACKAGEPATH;
+        String webrootPackagePath = getInitParameter(INITPARAM_WEBROOTPACKAGEPATH);
+        if (webrootPackagePath == null) {
+            webrootPackagePath = DEFAULT_WEBROOTPACKAGEPATH;
         }
-        if (rootPackagePath.endsWith("/")) {
-            rootPackagePath = rootPackagePath.substring(0, rootPackagePath
-                    .length() - 1/*= "/".length() */);
+        if (webrootPackagePath.endsWith("/")) {
+            webrootPackagePath = webrootPackagePath.substring(0,
+                    webrootPackagePath.length() - 1/*= "/".length() */);
         }
-        rootPackagePath_ = rootPackagePath;
+        webrootPackagePath_ = webrootPackagePath;
+
+        String zptrootPackagePath = getInitParameter(INITPARAM_ZPTROOTPACKAGEPATH);
+        if (zptrootPackagePath == null) {
+            zptrootPackagePath = DEFAULT_ZPTROOTPACKAGEPATH;
+        }
+        if (zptrootPackagePath.endsWith("/")) {
+            zptrootPackagePath = zptrootPackagePath.substring(0,
+                    zptrootPackagePath.length() - 1/*= "/".length() */);
+        }
+        zptrootPackagePath_ = zptrootPackagePath;
 
         resourceEncoding_ = getInitParameter(INITPARAM_RESOURCEENCODING);
         if (resourceEncoding_ == null) {
@@ -56,8 +73,11 @@ public class ScaffoldingYmirFreyjaServlet extends YmirFreyjaServlet {
 
     @Override
     protected TemplateSet createTemplateSet() {
-        return new CascadingTemplateSet(super.createTemplateSet(),
-                new TraversingJavaResourceTemplateSet(rootPackagePath_,
-                        resourceEncoding_, getTemplateEvaluator()));
+        return new CascadingTemplateSet(new TemplateSet[] {
+            super.createTemplateSet(),
+            new JavaResourceTemplateSet(webrootPackagePath_, resourceEncoding_,
+                    getTemplateEvaluator()),
+            new TraversingJavaResourceTemplateSet(zptrootPackagePath_,
+                    resourceEncoding_, getTemplateEvaluator()) });
     }
 }
